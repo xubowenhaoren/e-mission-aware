d7795ead42ec6705e9693baaf5ff0dc2bb078ccd
bowenxu <xubowenhaoren@gmail.com>
2020-04-25 14:26:35 -0700

    Update plugin.xml to temporarily remove the Aware service. Update the AwarePlugin to remove redundant lines.

--- AwarePlugin/src/android/AwarePlugin.java
+++ AwarePlugin/src/android/AwarePlugin.java
@@ -1,4 +1,4 @@
-package edu.berkeley.eecs.emission.cordova.aware;
+package edu.berkeley.eecs.emission.cordova.aware.AwarePlugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -7,19 +7,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.Manifest;
-import android.support.v4.content.PermissionChecker;
+//import android.support.v4.content.PermissionChecker;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import com.aware.Applications;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
+import edu.berkeley.eecs.emission.cordova.unifiedlogger.Log;
+
+import java.util.ArrayList;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class AwarePlugin extends CordovaPlugin {
-    private ArrayList<String> REQUIRED_PERMISSIONS = new ArrayList<>();
+//    private ArrayList<String> REQUIRED_PERMISSIONS = new ArrayList<>();
     public static final String TAG = "AwarePlugin";
 
     @Override
@@ -41,6 +44,7 @@ public class AwarePlugin extends CordovaPlugin {
                 Manifest.permission.READ_SYNC_STATS
         };
         cordova.requestPermissions(this, 0, permissions);
+        Log.d(cordova.getActivity(), TAG, "permissions for Aware OK");
 //        AwareService service = new AwareService();
 //        service.start();
     }
@@ -50,9 +54,25 @@ public class AwarePlugin extends CordovaPlugin {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
+            pluginInitialize();
             return true;
         }
+
+        if (action.equals("performAdd")) {
+            int arg1 = args.getInt(0);
+            int arg2 = args.getInt(1);
+            /* Indicating success is failure is done by calling the appropriate method on the
+            callbackContext.*/
+            int result = arg1 + arg2;
+            callbackContext.success("result calculated in Java: " + result);
+            return true;
+        }
+
+        if (action.equals("manual_sync")) {
+            // manual sync ops here
+        }
         return false;
+
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
