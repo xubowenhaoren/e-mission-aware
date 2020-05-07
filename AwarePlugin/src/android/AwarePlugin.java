4368554989d9e2bb2500391494298fcdc62f4c30
bowenxu <xubowenhaoren@gmail.com>
2020-05-07 01:44:20 -0700

    update awareplugin

--- AwarePlugin/src/android/AwarePlugin.java
+++ AwarePlugin/src/android/AwarePlugin.java
@@ -1,5 +1,6 @@
-package edu.berkeley.eecs.emission.cordova.aware.AwarePlugin;
+package edu.berkeley.eecs.emission.cordova.aware;
 
+import org.apache.cordova.BuildConfig;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -7,10 +8,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.Manifest;
-//import android.support.v4.content.PermissionChecker;
+//import androidx.core.content.PermissionChecker;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
+
 import com.aware.Applications;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
@@ -22,11 +25,13 @@ import java.util.ArrayList;
  * This class echoes a string called from JavaScript.
  */
 public class AwarePlugin extends CordovaPlugin {
-//    private ArrayList<String> REQUIRED_PERMISSIONS = new ArrayList<>();
+    //    private ArrayList<String> REQUIRED_PERMISSIONS = new ArrayList<>();
     public static final String TAG = "AwarePlugin";
+    private Context ctxt;
 
     @Override
     public void pluginInitialize() {
+        ctxt = cordova.getActivity().getApplicationContext();
         String[] permissions = {
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -44,9 +49,37 @@ public class AwarePlugin extends CordovaPlugin {
                 Manifest.permission.READ_SYNC_STATS
         };
         cordova.requestPermissions(this, 0, permissions);
-        Log.d(cordova.getActivity(), TAG, "permissions for Aware OK");
+        Log.d(ctxt, TAG, "permissions for Aware OK");
 //        AwareService service = new AwareService();
 //        service.start();
+        Aware.setSetting(cordova.getActivity().getApplicationContext(),
+                Aware_Preferences.DEVICE_LABEL, "Bowen - 0502");
+        if (!Aware.isStudy(ctxt)) {
+            joinStudy();
+            Log.d(ctxt, TAG, "joinStudy OK");
+        }
+
+//        else {
+//          Log.d(ctxt, TAG, "checking battery");
+//          Applications.isAccessibilityServiceActive(ctxt.getApplicationContext());
+//          Aware.isBatteryOptimizationIgnored(ctxt.getApplicationContext(),
+//            ctxt.getApplicationContext().getPackageName());
+//        }
+        try {
+            JSONObject installedVersion = new JSONObject();
+            installedVersion.put("package_installed", ctxt.getPackageName());
+            installedVersion.put("version_name", BuildConfig.VERSION_NAME);
+            installedVersion.put("version_code", BuildConfig.VERSION_CODE);
+            AwareDebug(installedVersion);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (!Aware.IS_CORE_RUNNING) {
+            StartAware();
+            Log.d(ctxt, TAG, "StartAware OK? " + Aware.IS_CORE_RUNNING);
+        } else {
+            Log.d(ctxt, TAG, "Aware already started");
+        }
     }
 
     @Override
@@ -56,16 +89,72 @@ public class AwarePlugin extends CordovaPlugin {
             Integer num2 = args.getInt(1);
             this.sum(num1, num2, callbackContext);
             return true;
+        } else if (action.equals("checkBattery")) {
+            Log.d(ctxt, TAG, "checking battery");
+            Applications.isAccessibilityServiceActive(ctxt.getApplicationContext());
+            Aware.isBatteryOptimizationIgnored(ctxt.getApplicationContext(),
+                    ctxt.getApplicationContext().getPackageName());
+            Log.d(ctxt, TAG, "Is study: " + Aware.isStudy(ctxt)
+                    + ", is core running: " + Aware.IS_CORE_RUNNING);
+//            if (!Aware.isStudy(ctxt)) {
+//              joinStudy();
+//              Log.d(ctxt, TAG, "joinStudy OK");
+//            }
+            callbackContext.success("checked battery");
+            return true;
         }
+
         return false;
 
     }
+
 
     private void sum(Integer num1, Integer num2, CallbackContext callbackContext) {
         if(num1 != null && num2 != null) {
             callbackContext.success(num1 + num2);
         } else {
             callbackContext.error("Expected two integer arguments.");
+        }
+    }
+
+    class JoinStudyTask extends AsyncTask<Void,Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... params) {
+            Aware.joinStudy(cordova.getActivity().getApplicationContext(),
+                    "https://slicomex.cs.washington.edu/index.php/webservice/index/12/c0MlRvLaJPKJ");
+            return null;
+        }
+    }
+
+    private void joinStudy() {
+        new JoinStudyTask().execute();
+    }
+
+    private void StartAware(){
+        new StartAwareTask().execute();
+    }
+
+    class StartAwareTask extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... params) {
+            Intent aware = new Intent(ctxt, Aware.class);
+            ctxt.startService(aware);
+            return null;
+        }
+    }
+
+    private void AwareDebug(JSONObject JsOb) {
+        new AwareDebugTask().execute(JsOb);
+    }
+
+    class AwareDebugTask extends AsyncTask<JSONObject, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(JSONObject... jsonObjects) {
+            Aware.debug(ctxt, jsonObjects.toString());
+            return null;
         }
     }
 }
--- AwarePlugin/www/AwarePlugin.js
+++ AwarePlugin/www/AwarePlugin.js
@@ -16,3 +16,7 @@ var exec = require('cordova/exec');
 window.sum = function(num1, num2, successCallback, errorCallback) {
 	cordova.exec(successCallback, errorCallback, "AwarePlugin", "sum", [num1, num2]);
 };
+
+window.checkBattery = function(successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "AwarePlugin", "checkBattery");
+};
